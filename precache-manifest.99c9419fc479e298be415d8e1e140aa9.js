9d9dd5abc5d52202e24bd6c86f3a236c540386ab
Soul1 <darkwaud@gmail.com>
2020-03-23 20:57:21 +0300

    Updates

---
+++ precache-manifest.99c9419fc479e298be415d8e1e140aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f17f8bd93f03cd7da4f7968b5da58a0",
+    "url": "/mini-learn-project/index.html"
+  },
+  {
+    "revision": "cb41734f23daf3ee01ef",
+    "url": "/mini-learn-project/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "a0b216fd6536cd24b4da",
+    "url": "/mini-learn-project/static/js/2.3634e2bb.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/mini-learn-project/static/js/2.3634e2bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb41734f23daf3ee01ef",
+    "url": "/mini-learn-project/static/js/main.39c68c6b.chunk.js"
+  },
+  {
+    "revision": "14573c04e4b0da46c258",
+    "url": "/mini-learn-project/static/js/runtime-main.815415fe.js"
+  }
+]);
