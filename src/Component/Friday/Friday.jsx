bf802f4c98a43fb3b7a33b5fd93f9f7dcd18ad36
Soul1 <darkwaud@gmail.com>
2020-03-26 23:22:01 +0300

    change in README and in not which componets

--- src/Component/Friday/Friday.jsx
+++ src/Component/Friday/Friday.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import MyName from "../MyName/MyName";
-import MySkills from "../../Containers/MySkills/MySkills";
-import NamesMyFriends from "../../Containers/myFriends/namesMyFriends";
-
 const Friday = () => {
   return (
     <div>
-      <div className='app__info-me'>
-        <MyName/>
-        <MySkills/>
-      </div>
-      <div className='app__info-friends'>
-        <NamesMyFriends/>
-      </div>
+      Friday
     </div>
   );
 };
